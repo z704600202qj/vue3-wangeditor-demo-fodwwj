c3c322b9c2bd5cf06cc29b8c2c32a18d2cc8cf1e
z704600202qj <704700551@qq.com>
2022-10-27 14:28:11 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue3-wangeditor-demo-fodwwj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue3-wangeditor-demo-fodwwj)
---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <BasicEditor></BasicEditor>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import BasicEditor from './components/BasicEditor.vue';
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    BasicEditor,
+  },
+};
+</script>
+
+<style></style>
